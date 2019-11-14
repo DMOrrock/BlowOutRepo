ec8b13c1c0174f7fe2c11253b13f8d9530ae0bef
MyMenus <dorrock3@byu.edu>
2019-11-13 17:37:18 -0700

    Add project files.

---
+++ BlowOut/Controllers/ContactController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BlowOut.Controllers
+{
+    public class ContactController : Controller
+    {
+        // GET: Contact
+        public ActionResult Index()
+        {
+            ViewBag.outputString = "Please call Support at <strong><u>801-555-1212</u></strong>. Thank you!";
+            return View();
+        }
+
+        public ActionResult Email(string name, string email)
+        {
+            ViewBag.EmailOutput = "Thank you " + name + ". We will send you an email shortly to the address: " + email;
+            return View();
+        }
+    }
+}
---
+++ BlowOut/Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BlowOut.Controllers
+{
+    public class HomeController : Controller
+    {
+        public static Dictionary<string, int> newDict = new Dictionary<string, int>
+        {
+            {"Trumpet",55},
+            {"Trombone",60},
+            {"Tuba",70},
+            {"Flute",45},
+            {"Clarinet",35},
+            {"Saxophone",42}
+        };
+
+        public static Dictionary<string, int> usedDict = new Dictionary<string, int>
+        {
+            {"Trumpet",25},
+            {"Trombone",35},
+            {"Tuba",50},
+            {"Flute",25},
+            {"Clarinet",27},
+            {"Saxophone",30}
+        };
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Rentals()
+        {
+            return View();
+        }
+
+        public ActionResult UsedRentals()
+        {
+            ViewBag.displayDictionary = usedDict;
+            return View();
+        }
+
+        public ActionResult NewRentals()
+        {
+            ViewBag.displayDictionary = newDict;
+            return View();
+        }
+    }
+}
